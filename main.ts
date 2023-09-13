814d1480c780bcd77f43318e8ab808f2fe5755eb
Mauricio de Mello <mau-ideias@hotmail.com>
2023-09-13 01:19:38 -0300

    primeiro commit

---
+++ main.ts
@@ -0,0 +1,13 @@
+function multiplicacao(numero: number, numero1: number): number {
+    return numero * numero1;
+}
+
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+const resultadoMultiplicacao = multiplicacao(2, 3); 
+const saudacaoParaEbac = saudacao("Tutor Caio Lima"); 
+
+console.log(resultadoMultiplicacao)
+console.log(saudacaoParaEbac)
